6709884d4cb9dc12d585780dd643efc52aee7c97
Matlhatsi Mogale <mogalegoodness6@gmail.com>
2026-04-13 15:09:26 +0200

    Added the colour for the title and for the name text

--- PROGPOEP1/Program.cs
+++ PROGPOEP1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace PROGPOEP1
 { 
@@ -13,22 +14,30 @@ class Program
 {
     static void Main()
     {
+
+
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("      ██████ ██    ██ ██████  ███████ ██████  ███████ ███████  ██████ ██    ██ ██████  ██ ████████ ██    ██     ██████   ██████  ████████ \r\n    ██       ██  ██  ██   ██ ██      ██   ██ ██      ██      ██      ██    ██ ██   ██ ██    ██     ██  ██      ██   ██ ██    ██    ██    \r\n    ██        ████   ██████  █████   ██████  ███████ █████   ██      ██    ██ ██████  ██    ██      ████       ██████  ██    ██    ██    \r\n    ██         ██    ██   ██ ██      ██   ██      ██ ██      ██      ██    ██ ██   ██ ██    ██       ██        ██   ██ ██    ██    ██    \r\n     ██████    ██    ██████  ███████ ██   ██ ███████ ███████  ██████  ██████  ██   ██ ██    ██       ██        ██████   ██████     ██    \r\n                                                                                                                                         \r\n                                                                                                                                         ");
         Console.Title = " Tlhatsi Cybersecurity Awareness Bot";
 
         // Play voice greeting
-        AudioPlayer.PlayGreeting();
+        CyberSecurityBot.AudioPlayer.PlayGreeting();
+        
 
         // Show ASCII UI
         UIHelper.ShowHeader();
 
         // Ask user name
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
+        Console.ResetColor();
 
         while (string.IsNullOrEmpty(name))
         {
-            Console.Write("Name  field cannot be empty. Kindly enter your Name" +
-                "nter your name: ");
+            Console.Write("Name  field cannot be empty. Kindly enter your Name");
+               
             name = Console.ReadLine();
         }
 
